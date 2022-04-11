5d30d5d79571bdda6ee1096303803370c7f408c3
Ying091909 <72859301+Ying091909@users.noreply.github.com>
2022-04-11 10:21:35 +0800

    Create test.py

---
+++ test.py
@@ -0,0 +1,10 @@
+def edit_distance(str1, str2):
+    edit = [[i + j for j in range(len(str2) + 1)] for i in range(len(str1) + 1)]
+    for i in range(1, len(str1) + 1):
+        for j in range(1, len(str2) + 1):
+            if str1[i - 1] == str2[j - 1]:
+                d = 0
+            else:
+                d = 1
+            edit[i][j] = min(edit[i - 1][j] + 1, edit[i][j - 1] + 1, edit[i - 1][j - 1] + d)
+    return edit[len(str1)][len(str2)]
